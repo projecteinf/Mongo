7a47dc0f3a92771ba3a841b88bf3120827cd855a
projecteinf <projecteinf@boscdelacoma.cat>
2023-11-22 08:57:59 -0700

    Get all properties from client connection

--- TestConnection/Program.cs
+++ TestConnection/Program.cs
@@ -1,6 +1,38 @@
-﻿using MongoDB.Driver;
+﻿using System.Collections;
+using System.Reflection;
+using MongoDB.Driver;
+
+void PrintProperties(object obj, string prefix = "")
+{
+    Type type = obj.GetType();
+    PropertyInfo[] properties = type.GetProperties();
+
+    foreach (PropertyInfo property in properties)
+    {
+        if (property!=null) {
+            object propValue = property.GetValue(obj, null);
+            if (propValue!=null) {
+                Console.WriteLine($"{prefix}{property.Name} = {propValue}");
+                if (property.Name == "Databases") {
+                    foreach (var db in (IEnumerable)propValue) {
+                        PrintProperties(db, prefix + "  ");
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+
+
 
 var client = new MongoClient("mongodb://localhost:27017");
 
+Type type = client.GetType();
+PropertyInfo[] properties = type.GetProperties();
 
-Console.WriteLine(client);
+PrintProperties(client);
+
+
+
--- TestConnection/README.md
+++ TestConnection/README.md
@@ -1,6 +1,12 @@
 # Mongo
 Creació contenidor mongo
+```bash
+    cd Docker
+    docker-compose up
 ```
-cd Docker
-docker-compose up
+# Instal·lació drivers
+```bash
+    dotnet add package MongoDB.Driver
 ```
+
+
