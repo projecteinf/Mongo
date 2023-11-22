681e64694edfe0593a6c66c8b9a00b30d0dd3967
projecteinf <projecteinf@boscdelacoma.cat>
2023-11-22 11:26:58 -0700

    Afegida classe CRUD

---
+++ TestConnection/CRUD.cs
@@ -0,0 +1,19 @@
+using MongoDB.Driver;
+
+static class CRUD {
+    public static void Create(MongoClient mongoClient) {
+        
+    }
+
+    public static void Read() {
+        // ...
+    }
+
+    public static void Update() {
+        // ...
+    }
+
+    public static void Delete() {
+        // ...
+    }
+}
