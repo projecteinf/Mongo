cceaeae0cbe50342c7358c608c843846bc699059
projecteinf <projecteinf@boscdelacoma.cat>
2023-12-04 11:45:25 -0700

    Identificar totes les classes que s'utilitzen dins d'un array

--- BooksLibrary/Classes/Model/Classes/Prestecs.cs
+++ BooksLibrary/Classes/Model/Classes/Prestecs.cs
@@ -1,5 +1,11 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace mba.BooksLibrary.Model {
     public class Prestecs {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; } = MongoDB.Bson.ObjectId.GenerateNewId().ToString();
         public string LibraryId { get; set; } = null!;
         public string UserId { get; set; } = null!;
         public DateTime? Date { get; set; } = null!;
--- BooksLibrary/Classes/Model/Classes/User.cs
+++ BooksLibrary/Classes/Model/Classes/User.cs
@@ -1,7 +1,13 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace mba.BooksLibrary.Model
 {
     public class User
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; } = MongoDB.Bson.ObjectId.GenerateNewId().ToString();
         public string Name { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
