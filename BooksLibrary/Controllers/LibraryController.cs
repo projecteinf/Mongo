22b9e543ed4f0b3785b1d1ca07b621818167d2a3
projecteinf <projecteinf@boscdelacoma.cat>
2023-12-06 02:26:42 -0700

    Versió API

--- BooksLibrary/Controllers/LibraryController.cs
+++ BooksLibrary/Controllers/LibraryController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace mba.BooksLibrary.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/v1/[controller]")]
 public class LibraryController : ControllerBase
 {
     private readonly LibraryService _libraryService;
